61357975ac16731a7acf38ebee67256019c28881
Srujana2406 <129171029+Srujana2406@users.noreply.github.com>
2023-03-29 06:57:56 +0530

    Add files via upload

---
+++ class 1.4.cpp
@@ -0,0 +1,36 @@
+/*
+ * C Program to Check whether a given Number is strong Number using while loop
+ */
+ 
+#include <stdio.h>
+ 
+int factorial(int n)
+{
+    int i, fact = 1;
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+ 
+int main(void)
+{
+    int number, sum = 0;
+    printf("Enter the number: ");
+    scanf("%d", &number);
+    int i = n;
+    while (i != 0)
+    {
+        sum = sum + factorial(i % 10);
+        i = i / 10;
+    }
+    if (sum == n)
+    {
+        printf("%d is strong number", number);
+    }
+    else
+    {
+        printf("%d is not a strong number", number);
+    }
+}
